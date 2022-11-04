acd25f2d5940f2d72df16629b8a9d1b9855a383b
1JayeshPatil <jayeshpatil2224@gmail.com>
2022-11-04 07:41:58 +0530

    Updated todo

--- server.js
+++ server.js
@@ -2,15 +2,37 @@ const http = require("http");
 
 const port = 8081; // local port no
 
+const toDoList = ["Complete Node Byte", "Play Cricket"];
+
 http
     .createServer((req, res) => {
-        // callback function
-        res.writeHead(200, { "Content-Type": "text/html" });
-        res.write("<h1>Hello, this is from my server</h1>");
+        const { method, url } = req;            // callback function
+
+        if (url === "/todos") {
+            if (method === "GET") {
+                res.writeHead(200, { "Content-Type": "text/html" });
+                res.write(toDoList.toString());
+            } else if (method === 'POST') {
+                let body = '';
+                req.on('error', (err) => {
+                    console.error(err)
+                }).on('data', (chunk) => {
+                    body += chunk;
+                    console.log(chunk);
+                }).on("end", () => {
+                    body = JSON.parse(body);
+                    console.log("data: ", body);
+                })
+            } else {
+                res.writeHead(501);
+            }
+        } else {
+            res.writeHead(404);
+        }
+
         res.end();
     })
-    .listen(port, () => {   
-        //callback function
+    .listen(port, () => {                         //callback function
         console.log(`Nodejs server started on port ${port}`);
     });
 
